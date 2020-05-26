d4c4955e0081093b2721147d1445bb4532effe63
Foximus <back.fox@ya.ru>
2020-05-26 20:25:40 +0300

    add time.sleep in test

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 from selenium.common.exceptions import NoSuchElementException
+import time
 
 
 class TestSwitchLanguages:
@@ -8,6 +9,7 @@ class TestSwitchLanguages:
         link = f"http://selenium1py.pythonanywhere.com/{lang}/catalogue/coders-at-work_207/"
 
         browser.get(link)
+        time.sleep(30)
         try:
             assert browser.find_element_by_css_selector('form#add_to_basket_form button')
         except NoSuchElementException:
